d6d43df5df639fc2e8619324a9124e47dd47eaae
NatashaKul7 <natasha.kulibaba0703@gmail.com>
2023-08-04 22:11:56 +0200

    1task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+startBtn.addEventListener("click", () => {
+    startBtn.disabled = true;
+    timerId = setInterval(() => {
+       
+            const randomColor = getRandomHexColor();
+        document.body.style.background = randomColor;
+        
+  }, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+      startBtn.disabled = false;
+});
 
