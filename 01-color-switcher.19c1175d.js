b027a8f327f9e501cb446e094c189ca5b3e868dc
NatashaKul7 <125500989+NatashaKul7@users.noreply.github.com>
2023-08-04 20:12:24 +0000

    Deploying to gh-pages from @ NatashaKul7/goit-js-hw-09@d6d43df5df639fc2e8619324a9124e47dd47eaae 🚀

---
+++ 01-color-switcher.19c1175d.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(()=>{t.disabled=!0,timerId=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.background=t}),1e3)})),e.addEventListener("click",(()=>{clearInterval(timerId),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.19c1175d.js.map
