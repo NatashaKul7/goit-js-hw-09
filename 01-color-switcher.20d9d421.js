55a54dae45df1f371ee514d780d882ee41c96aac
NatashaKul7 <125500989+NatashaKul7@users.noreply.github.com>
2023-08-06 20:24:46 +0000

    Deploying to gh-pages from @ NatashaKul7/goit-js-hw-09@8e5eed943fba619ff095677e4c3aa8ffe8fc3688 🚀

---
+++ 01-color-switcher.20d9d421.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let a=null;t.addEventListener("click",(()=>{t.disabled=!0,a=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.background=t}),1e3)})),e.addEventListener("click",(()=>{clearInterval(a),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.20d9d421.js.map
--- 01-color-switcher.bb79206f.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,timerId=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));document.body.style.background=t}),1e3)})),e.addEventListener("click",(function(){clearInterval(timerId),t.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.bb79206f.js.map
